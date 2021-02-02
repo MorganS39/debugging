75892795cd0183f81ef1cee2e4b3123a46ed7842
Grumpybear39 <56695599+Grumpybear39@users.noreply.github.com>
2021-02-02 13:09:47 -1000

    console.log() best practices

---
+++ debug.js
@@ -0,0 +1,10 @@
+// Using console.log() effectively to find bugs easier
+let a = 5;
+let b = 1;
+a++;
+console.log(a); //This log is added to ensure the value of a is correct
+
+let sumAB = a + b;
+console.log(sumAB);
+
+//
